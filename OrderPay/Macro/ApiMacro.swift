2e6ab06484b0a43c8d1fdc9c0b036c2b1619ffdb
answerzou <1142017183@qq.com>
2017-12-27 22:26:38 +0800

    第三方键盘中文显示“完成”

--- OrderPay/Macro/ApiMacro.swift
+++ OrderPay/Macro/ApiMacro.swift
@@ -18,21 +18,16 @@ let APP_SERVER_API = "http://app.buyijinfu.com/xdyAppServer"
 //--------用户登录及相关接口--------//
 ///登录
 let API_POST_LOGIN         = APP_SERVER_API+"/api/APPBizRest/appLogin/v1/"
-
 ///注册
 let API_POST_REGISTER         = APP_SERVER_API+"/api/APPBizRest/appRegister/v1/"
 ///验证码
 let API_POST_VERIFICATION     = APP_SERVER_API+"/api/APPBizRest/sendSMCode/v1/"
-
-//---------保存用户通讯录----------//
+///保存用户通讯录
 let API_POST_SAVEADDRESSBOOKS     = APP_SERVER_API+"/api/APPBizRest/saveAddressBooks/v1/"
-
-//---------实名认证----------//
+///实名认证
 let API_POST_APPAUTH     = APP_SERVER_API+"/api/APPBizRest/appAuth/v1/"
-
 ///我的订单
 let API_POST_MYGETLIST     = APP_SERVER_API+"/api/APPBizRest/queryMyGetList/v1/"
-
 ///版本控制
 let API_POST_UPAPPVERSION     = APP_SERVER_API+"/api/APPBizRest/goUpAppVersion/v1/"
 
